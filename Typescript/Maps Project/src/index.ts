961b0dc717af2d86ea9bad786e0a7bb5ab4a591b
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-14 14:00:57 +0530

    Adding CustomMap Class

--- Typescript/Maps Project/src/index.ts
+++ Typescript/Maps Project/src/index.ts
@@ -2,17 +2,12 @@
 
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 // const user = new User();
 // const company = new Company();
+const customMap = new CustomMap('map');
 
 // console.log(user);
 // console.log(company);
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
