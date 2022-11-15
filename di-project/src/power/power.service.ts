0ad50d53ff46dc12698f56a10194a349a45f2f51
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-15 12:47:16 +0530

    Car Value Estimate Project Initial Setup

--- di-project/src/power/power.service.ts
+++ di-project/src/power/power.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PowerService {
   supplyPower(watts: number) {
-    console.log(`Supplying ${watts} worth of power.s`);
+    console.log(`Supplying ${watts} worth of power.`);
   }
 }
