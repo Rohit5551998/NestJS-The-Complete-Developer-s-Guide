1b79790ec262a0c697c836bbaf588813f10cab60
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-15 17:38:30 +0530

    Check for Existing User Email

--- car-value-estimate/src/users/auth.service.ts
+++ car-value-estimate/src/users/auth.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
 
 @Injectable()
 export class AuthService {
   constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // Check if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in Use');
+    }
+
+    // Hash the users password
+
+    // Create a New User
+
+    // Return the User
+  }
+
+  signin(email: string, password: string) {
+
+  }
 }
