86a77db39a83c4dcebb2a530f78abc9927c8a3ad
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-15 12:25:55 +0530

    DI for Disk Module

--- di-project/src/cpu/cpu.service.ts
+++ di-project/src/cpu/cpu.service.ts
@@ -4,4 +4,10 @@ import { PowerService } from '../power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log(`Drawing ${a + b} watts of power from Power Service`);
+    this.powerService.supplyPower(a + b);
+    return a +  b;
+  }
 }
