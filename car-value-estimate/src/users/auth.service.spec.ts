041cf83d45e0652b4c601c51090fb6f028372c66
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 12:09:44 +0530

    More Intelligent Tests

--- car-value-estimate/src/users/auth.service.spec.ts
+++ car-value-estimate/src/users/auth.service.spec.ts
@@ -9,11 +9,26 @@ describe('AuthService', () => {
   let fakeUsersService: Partial<UsersService>
 
   beforeEach(async () => {
+    const users: User[] = [];
+
     // Create fake copy of Users Service
+    // fakeUsersService = {
+    //   find: () => Promise.resolve([]),
+    //   create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
+    // }
+    
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter(user => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {id: Math.floor(Math.random() * 999999), email, password} as User
+        users.push(user);
+        return Promise.resolve(user);
+      }
     }
+    
   
     const module = Test.createTestingModule({
       providers: [
@@ -43,13 +58,15 @@ describe('AuthService', () => {
 
   // it ('Throws an error if user signs up with an email that is in use', async (done) => {
   it ('Throws an error if user signs up with an email that is in use', async  () => {
-    fakeUsersService.find = () => Promise.resolve([{id: 1, email: 'a', password: 'b'} as User]);
+    // Without More Intelligent tests
+    // fakeUsersService.find = () => Promise.resolve([{id: 1, email: 'a', password: 'b'} as User]);
     // Old way
     // try {
     //   await service.signup('test@test.com', 'test@123');
     // } catch(err) {
     //   done();
     // }
+    await service.signup('test@test.com', 'test@123');
     await expect(service.signup('test@test.com', 'test@123')).rejects.toThrow(BadRequestException,);
   });
 
@@ -60,16 +77,21 @@ describe('AuthService', () => {
   });
 
   it ('Throws an error if an Invalid password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{id: 1, email: 'test@test.com', password: 'test'} as User])
+    // Without More Intelligent tests
+    // fakeUsersService.find = () =>
+    //   Promise.resolve([{id: 1, email: 'test@test.com', password: 'test'} as User])
+    await service.signup('test@test.com', 'password');
     await expect(
-      service.signin('test@test.com', 'password')
+      service.signin('test@test.com', 'password1')
     ).rejects.toThrow(BadRequestException);
   });
 
   it ('Returns a password if correct password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{id: 1, email: 'test@test.com', password: 'test'} as User])
+    // Without More Intelligent tests
+    // fakeUsersService.find = () =>
+    //   Promise.resolve([{id: 1, email: 'test@test.com', password: 'test'} as User])
+    await service.signup('test@test.com', 'test');
+
     const user = await service.signin('test@test.com', 'test');
     expect(user).toBeDefined();
   });
