6974435304ebaf1211107057e3d646f2b7abf55f
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 13:47:43 +0530

    Applying Global Cookie Session and Pipes

--- car-value-estimate/src/app.module.ts
+++ car-value-estimate/src/app.module.ts
@@ -1,16 +1,20 @@
 import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module, ValidationPipe } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
+    // Simple Technique to switch database while testing
+    // database: process.env.NODE_ENV === 'test'
+    //   ? 'test.sqlite' : 'db.sqlite',
     database: 'db.sqlite',
     entities: [User, Report],
     synchronize: true,
@@ -26,4 +30,13 @@ import { Report } from './reports/report.entity';
     }
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['randomteststring']
+        }), 
+      ).forRoutes('*');
+  }
+}
