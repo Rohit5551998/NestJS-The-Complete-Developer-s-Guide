c08bda68130a922e05baaf59ffff800048241b01
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 17:06:37 +0530

    Association Decorators

--- car-value-estimate/src/users/user.entity.ts
+++ car-value-estimate/src/users/user.entity.ts
@@ -1,4 +1,5 @@
-import { AfterInsert, AfterRemove, AfterUpdate,Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
+import { Report } from "../reports/report.entity";
 // import { Exclude } from 'class-transformer';
 
 @Entity()
@@ -12,6 +13,9 @@ export class User {
   @Column()
   // @Exclude()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
