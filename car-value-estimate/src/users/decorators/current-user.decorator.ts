86ca1ebfa7c2641c006e302a08f34d7072ff76b2
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 09:45:16 +0530

    Reading session data in custom decorator

--- car-value-estimate/src/users/decorators/current-user.decorator.ts
+++ car-value-estimate/src/users/decorators/current-user.decorator.ts
@@ -1,7 +1,10 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    return 'Hi There';
+  // (data: any, context: ExecutionContext) => {
+  // Since no data will be supplied to Decorator changing it's type to never
+  (data: never, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.session.userId;
   }
 );
