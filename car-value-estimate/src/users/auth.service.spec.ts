daf5be5dc380b711f7fcb0764145113562347726
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 11:12:01 +0530

    Using Typescript to find & resolve type errors

--- car-value-estimate/src/users/auth.service.spec.ts
+++ car-value-estimate/src/users/auth.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test } from "@nestjs/testing";
+import { User } from "./user.entity";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 
 it ('Can Create an Instance of Auth Service', async () => {
   // Create fake copy of Users Service
-  const fakeUsersService = {
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({id: 1, email, password}),
+    create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
   }
 
   const module = Test.createTestingModule({
