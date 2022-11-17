279b9d463792438529336c61eb8621f224a16ecb
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 12:52:32 +0530

    Adding more tests to Users Controller

--- car-value-estimate/src/users/users.controller.spec.ts
+++ car-value-estimate/src/users/users.controller.spec.ts
@@ -1,8 +1,9 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
 import { User } from './user.entity';
-import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -25,8 +26,12 @@ describe('UsersController', () => {
       },
     };
     fakeAuthService = {
-      // signup: () => {},
-      // signin: () => {},
+      signup: (email: string, password: string) => {
+        return Promise.resolve({id: 1, email, password} as User);
+      },
+      signin: (email: string, password: string) => {
+        return Promise.resolve({id: 1, email, password} as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -49,4 +54,29 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('Should Find all Users with the given email', async () => {
+    const users = await controller.findAllUsers('test@test.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('test@test.com');
+  });
+
+  it('Should Find the User with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined
+    expect(user.id).toEqual(1);
+  });
+
+  it('Throws and Error if User with the given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('Sign In Updates Session Object and Returns User', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin({email: 'test@test.com', password: 'test'}, session);
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+  });
+
 });
