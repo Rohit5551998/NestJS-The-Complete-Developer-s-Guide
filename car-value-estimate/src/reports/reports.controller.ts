ec04ae8f8e03749f34f34a05d85bd4afb8a28e08
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-17 17:45:27 +0530

    Changes for removing excess user data

--- car-value-estimate/src/reports/reports.controller.ts
+++ car-value-estimate/src/reports/reports.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
 import { User } from '../users/user.entity';
+import { ReportDto } from './dtos/report.dto';
 import { AuthGuard } from '../guards/auth.guard';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 
 @Controller('reports')
@@ -12,7 +14,14 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
   }
+
+  @Patch('/:id')
+  approvedReport(@Param('id') id: string, @Body() body: ApprovedReportDto) {
+
+  }
+
 }
