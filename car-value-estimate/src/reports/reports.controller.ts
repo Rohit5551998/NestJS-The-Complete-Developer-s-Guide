52b70ab060ff8ecd07f32dad47f0dd3453d2e72f
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-18 13:24:27 +0530

    New Route To Get Estimate along with DTO

--- car-value-estimate/src/reports/reports.controller.ts
+++ car-value-estimate/src/reports/reports.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { AuthGuard } from '../guards/auth.guard';
 import { AdminGuard } from '../guards/admin.gurad';
 import { ReportsService } from './reports.service';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ApprovedReportDto } from './dtos/approved-report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
@@ -13,6 +14,11 @@ import { CurrentUser } from '../users/decorators/current-user.decorator';
 export class ReportsController {
 
   constructor(private reportsService: ReportsService) { }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
 
   @Post()
   @UseGuards(AuthGuard)
