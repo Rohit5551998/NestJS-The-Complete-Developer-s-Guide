2aa713642c32da564dccabc2703abaffdc95d0a8
Rohit Chawhan <rohitchawhan555@gmail.com>
2022-11-21 11:31:50 +0530

    Final changes

--- car-value-estimate/src/config/typeorm.config.ts
+++ car-value-estimate/src/config/typeorm.config.ts
@@ -7,12 +7,40 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) { }
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'sqlite',
-      // synchronize: false,
-      synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
-      database: this.configService.get<string>('DB_NAME'),
-      autoLoadEntities: true,
+    if (this.configService.get<boolean | undefined>('MIGRATIONS_RUN') === undefined) {
+      return {
+        type: this.configService.get<any>('DB_TYPE'),
+        // synchronize: false,
+        synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
+        database: this.configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+      }
+    }
+
+    else if (this.configService.get<boolean | undefined>('MIGRATIONS_RUN') === true) {
+      if (this.configService.get<boolean | undefined>('SSL') === undefined) {
+        return {
+          type: this.configService.get<any>('DB_TYPE'),
+          // synchronize: false,
+          synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          migrationsRun: true,
+        }
+      }
+      else if (this.configService.get<boolean | undefined>('SSL') === false) {
+        return {
+          type: this.configService.get<any>('DB_TYPE'),
+          // synchronize: false,
+          synchronize: this.configService.get<boolean>('SYNCHRONIZE'),
+          database: this.configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          migrationsRun: true,
+          ssl: {
+            rejectUnauthorized: false,
+          }
+        }
+      }
     }
   }
 }
